d6e157b29a615936199c8704365763f1696e9812
Oskar Fransson <oskar.leonard@hotmail.com>
2024-01-09 20:07:57 +0100

    tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+// @ts-expect-error - no types
+import nativewind from 'nativewind/preset';
+import type { Config } from 'tailwindcss';
+
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './app/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+    './modules/**/*.{js,jsx,ts,tsx}',
+  ],
+  presets: [nativewind],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
